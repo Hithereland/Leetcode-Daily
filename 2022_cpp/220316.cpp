af771ca4881bcf3426415e9595bfb1615634cce8
Nowherechan <zzxy233@gmail.com>
2022-03-16 12:59:58 +0800

    add: 220316.cpp

---
+++ 2022_cpp/220316.cpp
@@ -0,0 +1,37 @@
+//
+// Created by z on 3/16/2022.
+//
+
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> st;
+
+        int ji = 0, pi = 0; // judge index, push index
+        while (ji < popped.size()) {
+            if (st.size() == 0) {
+                if (pi < pushed.size())
+                    st.push(pushed[pi]);
+                else
+                    return false;
+                pi++;
+            }
+            if (popped[ji] != st.top()) {
+                if (pi < pushed.size())
+                    st.push(pushed[pi]);
+                else
+                    return false;
+                pi++;
+            }
+            else {
+                ji++;
+                st.pop();
+            }
+        }
+
+        if (st.size() == 0)
+            return true;
+        else
+            return false;
+    }
+};
