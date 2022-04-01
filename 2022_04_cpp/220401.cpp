6dcd8a77becb9c4a1ba8eda0a8dbac8856bd2dbb
Nowherechan <zzxy233@gmail.com>
2022-04-01 17:51:11 +0800

    refactor: 220401.cpp

--- 2022_04_cpp/220401.cpp
+++ 2022_04_cpp/220401.cpp
@@ -1,17 +1,11 @@
 //
 // Created by z on 4/1/2022.
 // # 344
-
+// STL, wohoo !
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int n = s.size();
-        for (int i = 0; i < n/2; i++)
-        {
-            char tmp = s[i];
-            s[i] = s[n-1-i];
-            s[n-1-i] = tmp;
-        }
+        reverse(s.begin(), s.end());
         return;
     }
 };
